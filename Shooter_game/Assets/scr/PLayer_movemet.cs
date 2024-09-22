038bee6f8a96b9a0db83f0c285909c053133f3a3
LePhroger <132900761+LePhroger@users.noreply.github.com>
2024-09-22 23:17:32 +0200

    added some code

---
+++ Shooter_game/Assets/scr/PLayer_movemet.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEngine;
+
+public class PLayer_movemet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
